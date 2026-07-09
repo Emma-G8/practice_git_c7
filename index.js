fdf2b395f6f7e770045bca6a5c6d4cc012784d9c
OLATUNDE Emmanuel Abiodun <olatundeemmanuelabiodun@gmail.com>
2026-07-10 00:47:44 +0100

    changed running to reading

--- index.js
+++ index.js
@@ -144,6 +144,6 @@ setTimeout(() => {
     console.log("Yeah!!! Today's test gonna be Easy - Peasy");
 }, 2000);
 
-console.log('We are still writing the code after the try-catch block');
+console.log('We are still reading the code after the try-catch block');
 console.log('We are everywhere you go');
 console.log('We are MTN, The Yello Network');
